24c4aedafb533af76f9c766b6b25b1c046f3a26a
Yann Beaud <yann.beaud@gmail.com>
2015-03-29 23:33:56 +0200

    Added admin.py to a better views in website admin

---
+++ mangopay/admin.py
@@ -0,0 +1,90 @@
+# -*- coding: UTF-8 -*-
+
+from django.contrib import admin
+from mangopay.models import MangoPayUser, MangoPayDocument, MangoPayPage, MangoPayBankAccount, MangoPayWallet, MangoPayTransfer, MangoPayRefund, MangoPayPayIn, MangoPayCardRegistration, MangoPayCard, MangoPayPayOut, MangoPayBankAccount, MangoPayPage, MangoPayDocument, MangoPayWallet
+
+
+class MangoPayUserAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'user', 'type', 'first_name', 'last_name', 'country_of_residence',)
+
+admin.site.register(MangoPayUser, MangoPayUserAdmin)
+
+
+class MangoPayWalletAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_user', 'currency',)
+
+
+admin.site.register(MangoPayWallet, MangoPayWalletAdmin)
+
+class MangoPayDocumentAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_user', 'status')
+
+
+admin.site.register(MangoPayDocument, MangoPayDocumentAdmin)
+
+
+class MangoPayPageAdmin(admin.ModelAdmin):
+
+    list_display = ('document',)
+
+
+admin.site.register(MangoPayPage, MangoPayPageAdmin)
+
+
+class MangoPayBankAccountAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id',)
+
+
+admin.site.register(MangoPayBankAccount, MangoPayBankAccountAdmin)
+
+
+class MangoPayPayOutAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_user', 'mangopay_wallet', 'status')
+
+
+admin.site.register(MangoPayPayOut, MangoPayPayOutAdmin)
+
+
+class MangoPayCardAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'is_active', 'is_valid',)
+
+
+admin.site.register(MangoPayCard, MangoPayCardAdmin)
+
+
+class MangoPayCardRegistrationAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_user', 'mangopay_card',)
+
+
+admin.site.register(MangoPayCardRegistration, MangoPayCardRegistrationAdmin)
+
+
+class MangoPayPayIntAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_user', 'mangopay_wallet', 'mangopay_card', 'status', )
+
+
+admin.site.register(MangoPayPayIn, MangoPayPayIntAdmin)
+
+
+class MangoPayRefundAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_user', 'mangopay_pay_in', 'execution_date', 'status', )
+
+
+admin.site.register(MangoPayRefund, MangoPayRefundAdmin)
+
+
+class MangoPayTransferAdmin(admin.ModelAdmin):
+
+    list_display = ('mangopay_id', 'mangopay_debited_wallet', 'mangopay_credited_wallet', 'debited_funds', 'execution_date', 'status',)
+
+
+admin.site.register(MangoPayTransfer, MangoPayTransferAdmin)
